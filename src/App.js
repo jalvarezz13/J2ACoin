2a30af829a54faff1a56089c33784943102fb8b3
jalvarezz13 <javier.alvarez13@alu.uclm.es>
2022-02-14 22:42:14 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="heatmap-box">Heat Map</div>
+      <div className="crypto-ranking">Ranking de Cryptos</div>
+      <div className="exchanges-links">Links de Exchanges</div>
+    </div>
+  );
+}
+
+export default App;
