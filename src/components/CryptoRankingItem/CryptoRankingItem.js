6b9d49708a838ccf2251ffae87437e7a288215f2
jalvarezz13 <javier.alvarez13@alu.uclm.es>
2022-03-01 18:51:46 +0100

    Completing information in cryptoRanking

--- src/components/CryptoRankingItem/CryptoRankingItem.js
+++ src/components/CryptoRankingItem/CryptoRankingItem.js
@@ -8,14 +8,29 @@ const CryptoRankingItem = ({ item }) => {
 
   return (
     <div className="item-container">
-      <p>#</p>
-      <p>Logo (img)</p>
-      <p>Nombre</p>
-      <p>{itemData.symbol}</p>
-      <p>Price</p>
-      <p>Cambio %</p>
+      <p>{itemData.cmc_rank}</p>
+      <img className="coin-logo" src={`https://s2.coinmarketcap.com/static/img/coins/64x64/${itemData.id}.png`} alt={`Logo de ${itemData.name}`} />
+      <p>
+        {itemData.name}({itemData.symbol})
+      </p>
+      <p>{itemData.quote.USD.price.toFixed(4)}$</p>
+      <p className={itemData.quote.USD.percent_change_24h > 0 ? "change-value-positive" : "change-value-negative"}>
+        {parseFloat(itemData.quote.USD.percent_change_24h).toFixed(4)}%
+      </p>
       <p>Market cap?</p>
-      <p>Last 7 days (img)</p>
+      <div className="item-change-7d">
+        <div className="item-change-7d-text">
+          <p>Últimos 7 días</p>
+          <p className={itemData.quote.USD.percent_change_7d > 0 ? "change-value-positive" : "change-value-negative"}>
+            {parseFloat(itemData.quote.USD.percent_change_7d).toFixed(4)}%
+          </p>
+        </div>
+        <img
+          src={`https://s3.coinmarketcap.com/generated/sparklines/web/7d/2781/${itemData.id}.svg`}
+          alt={`Gráfico a 7 días de ${itemData.name}`}
+          className={itemData.quote.USD.percent_change_7d > 0 ? "graph-7d-positive" : "graph-7d-negative"}
+        />
+      </div>
     </div>
   )
 }
