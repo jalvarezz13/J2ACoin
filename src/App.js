f72aa170b5286818e8ecd9112438cdd03bf6dd6e
jalvarezz13 <javier.alvarez13@alu.uclm.es>
2022-03-06 19:42:22 +0100

    Modified native structure and created responsive for different devices

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
 import React from "react"
-import "./app.css"
 import CryptoRanking from "./components/CryptoRanking/CryptoRanking"
+import HeatMap from "./components/HeatMap/HeatMap"
 import logo from "./assets/logo.png"
+import gitHubLogo from "./assets/github.svg"
+import "./app.css"
 
 function App() {
   return (
     <>
       <header className="header">
-        <div className="header-info">
+        <div className="header-flex">
           <img className="header-logo" src={logo} alt="Logo J2A Coin"></img>
           <h1 className="header-title">J2A Coin</h1>
         </div>
-        <p>Algo...</p>
+        <a href="https://github.com/jalvarezz13/isi-J2ACoin" target={"_blank"} rel="noreferrer">
+          <div className="header-flex">
+            <img className="header-github-logo" src={gitHubLogo} alt="Git Hub icon"></img>
+            <p className="header-github-text">GitHub</p>
+          </div>
+        </a>
       </header>
       <div className="container">
-        <div className="heatmap-box">Heat Map</div>
         <div className="crypto-ranking">
           <h2>Ranking de Cryptos</h2>
           <CryptoRanking />
         </div>
+        <div className="heatmap-box">
+          <h2>HeatMap</h2>
+          <HeatMap />
+        </div>
         <div className="exchanges-links">Links de Exchanges</div>
       </div>
-      <footer>
-        <p>GitHub Repo ------------- Copyright ----------- Authors</p>
+      <footer className="footer">
+        <p className="footer-text">Copyright © 2022 J2ACoin. Todos los derechos reservados.</p>
       </footer>
     </>
   )
