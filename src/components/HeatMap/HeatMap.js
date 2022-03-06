a587238c4056e8f5e218258ad47d637763e7708e
jalvarezz13 <javier.alvarez13@alu.uclm.es>
2022-03-06 19:42:42 +0100

    New features added

--- src/components/HeatMap/HeatMap.js
+++ src/components/HeatMap/HeatMap.js
@@ -3,7 +3,20 @@ import React from "react"
 import "./heatMap.css"
 
 const HeatMap = () => {
-  return <div className="heat-map"></div>
+  return (
+    <div className="heat-map">
+      <div className="heat-map-1">1</div>
+      <div className="heat-map-2">2</div>
+      <div className="heat-map-3">3</div>
+      <div className="heat-map-4">4</div>
+      <div className="heat-map-5">5</div>
+      <div className="heat-map-6">6</div>
+      <div className="heat-map-7">7</div>
+      <div className="heat-map-8">8</div>
+      <div className="heat-map-9">9</div>
+      <div className="heat-map-10">10</div>
+    </div>
+  )
 }
 
 export default HeatMap
